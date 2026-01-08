3246239ba922b808d18ffe6a8a71b18d5a6ec126
asraful <asrafulasraful166@gmail.com>
2026-01-08 12:35:47 +0600

    problem 5 solve

---
+++ problem_5.js
@@ -0,0 +1,12 @@
+function removeDuplicates(arr){
+    let uniqueArr=[];
+
+    for(let num of arr){
+        if(!uniqueArr.includes(num)){
+            uniqueArr.push(num)
+        }
+    }
+    return uniqueArr;
+}
+
+console.log(removeDuplicates([1,2,2,3,4,4]))
