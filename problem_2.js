b8d8e1de150b0cdd05386056fe2c746f1d7d349e
asraful <asrafulasraful166@gmail.com>
2026-01-08 12:01:20 +0600

    problem 2 solve

---
+++ problem_2.js
@@ -0,0 +1,12 @@
+function countVowels(str){
+      let vowels="aeiou";
+      let count=0;
+
+      for(let char of str.toLowerCase()){
+        if(vowels.includes(char)){
+            count++
+        }
+      }
+      return count;
+}
+console.log(countVowels("pogramming"))
