960b4ce34960328129a4ff78e8ca46784604140b
asraful <asrafulasraful166@gmail.com>
2026-01-25 17:05:56 +0600

    firstUniqueChar Solve

---
+++ firstUniqueChar.js
@@ -0,0 +1,9 @@
+function firstUniqueChar(str){
+    for(let char of str){
+        if(str.indexOf(char)=== str.lastIndexOf(char)){
+            return char
+        }
+    }
+    return null
+}
+console.log(firstUniqueChar("javascript"))
