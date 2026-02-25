2efa70d68b09ebc212f14cded40ca70f91a14d6d
asraful <asrafulasraful166@gmail.com>
2026-02-25 23:29:38 +0600

    small function added

--- Conditional_logic.js
+++ Conditional_logic.js
@@ -5,3 +5,10 @@ if(!stock){
 }
 
 console.log("out from condition",!stock)
+
+function addSum(number1, number2){
+        const sum=number1+number2
+        return sum
+}
+
+console.log(addSum(10,5))
